ee47270ecb105ba9b66fcc07470f9fab1f707791
Rajkumar Chaudhari <rajkumr.gie@gmail.com>
2025-06-16 14:40:08 +0530

    ffff

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
-  eslint :{
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
     ignoreDuringBuilds: true,
   },
-  /* config options here */
 };
 
 export default nextConfig;
