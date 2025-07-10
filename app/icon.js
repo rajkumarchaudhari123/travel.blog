b6f769c1a3706383f8836feeb604455423a35bfc
Rajkumar Chaudhari <rajkumr.gie@gmail.com>
2025-07-10 22:34:21 +0530

    icon

---
+++ app/icon.js
@@ -0,0 +1,38 @@
+// icon.tsx or route.ts (inside /app/icon/route.ts)
+
+import { ImageResponse } from "next/og";
+import React from "react";
+
+export const runtime = "edge";
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 24,
+          backgroundColor: "black",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "white",
+        }}
+      >
+        Travelya
+      </div>
+    ),
+    {
+      width: size.width,
+      height: size.height,
+    }
+  );
+}
